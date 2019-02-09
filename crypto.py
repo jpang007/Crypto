6f30ece04abf111ef63e00d8773329cc13a3e75e
Jeremy <jpang007@ucr.edu>
2019-02-09 14:59:26 -0600

    Added bitcoin basic graph

--- crypto.py
+++ crypto.py
@@ -5,44 +5,43 @@ import numpy
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
-
-from pprint import pprint
+import plotly.graph_objs as go
+from datetime import datetime
 
 #calculates average gain and loss over a period (usually 14)
-def calcDailyGains(ClosingPrice, Period):
+def calcDailyGains(ClosingPrice, RSIPeriod):
     AverageGain = []
     AverageLoss = []
     CurrentChange = 0
-    for i, j in enumerate(range(0,Period - 1)):
+    for i, j in enumerate(range(0,RSIPeriod - 1)):
         Change = ClosingPrice[i + 1] - ClosingPrice[i]
         if Change > 0:
             AverageGain.append(Change)
         else:
             AverageLoss.append(Change)
         #current gain
-        if j == Period - 2:
+        if j == RSIPeriod - 2:
             if Change > 0:
                 CurrentChange = Change
             else:
                 CurrentChange = Change
 
-    TotalAverageGain = (sum(AverageGain) / Period)
-    TotalAverageLoss = (sum(AverageLoss) / Period)
+    TotalAverageGain = (sum(AverageGain) / RSIPeriod)
+    TotalAverageLoss = (sum(AverageLoss) / RSIPeriod)
 
     return TotalAverageGain, TotalAverageLoss, CurrentChange
 
 #calculates simple moving average over a period (usually 20)
 #returns values for middle upper and lower band
-def calcSMA(ClosingPrice, Period):
-    MiddleBand = (sum(ClosingPrice) / Period)
+def calcSMA(ClosingPrice, BandPeriod):
+    MiddleBand = (sum(ClosingPrice) / BandPeriod)
     StandardDeviation = numpy.std(ClosingPrice)
     UpperBand = MiddleBand + (StandardDeviation * 2)
     LowerBand = MiddleBand - (StandardDeviation * 2)
     return LowerBand, MiddleBand, UpperBand
 
 #Formula to calculate RSI using average gain and loss
-def calcRSI(TotalAverageGain, TotalAverageLoss, CurrentChange, Period):
+def calcRSI(TotalAverageGain, TotalAverageLoss, CurrentChange, RSIPeriod):
     CurrentGain = 0
     CurrentLoss = 0
     firstRSI = TotalAverageGain / abs(TotalAverageLoss)
@@ -55,53 +54,83 @@ def calcRSI(TotalAverageGain, TotalAverageLoss, CurrentChange, Period):
         CurrentLoss = CurrentChange
 
     #smoothRSI
-    smoothRSI = (((TotalAverageGain * (Period - 1)) + CurrentGain) / Period) / (((abs(TotalAverageLoss) * (Period - 1)) + CurrentLoss) / Period)
+    smoothRSI = (((TotalAverageGain * (RSIPeriod - 1)) + CurrentGain) / RSIPeriod) / (((abs(TotalAverageLoss) * (RSIPeriod - 1)) + CurrentLoss) / RSIPeriod)
     finalRSI = 100 - (100 / (1 + smoothRSI))
     return finalRSI
 
 
 #Get's the current price
-# currentprice = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD&api_key=0bfac81ab05f7e572f8cc18a28a05c6c2f3665a10d17099efe1d7c2e3b3e0195")
-# pricedata = currentprice.json()
-# print (pricedata)
+currentprice = requests.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD&api_key=0bfac81ab05f7e572f8cc18a28a05c6c2f3665a10d17099efe1d7c2e3b3e0195")
+pricedata = currentprice.json()
+print (pricedata)
 
-bitcoinHistorical = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=14&api_key=0bfac81ab05f7e572f8cc18a28a05c6c2f3665a10d17099efe1d7c2e3b3e0195")
+bitcoinHistorical = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=20&api_key=0bfac81ab05f7e572f8cc18a28a05c6c2f3665a10d17099efe1d7c2e3b3e0195")
 bitcoinHistoricalData = bitcoinHistorical.json()
 #OpeningPrice = []
-ClosingPrice = []
-Period = 14
-for i in range(0, Period):
+RSIClosingPrice = []
+BandClosingPrice = []
+RSIPeriod = 14
+BandPeriod = 20
+print (bitcoinHistoricalData['Data'])
+for i in range(0 + 6, RSIPeriod + 6):
     #OpeningPrice.append(bitcoinHistoricalData['Data'][i]['open'])
-    ClosingPrice.append(bitcoinHistoricalData['Data'][i]['close'])
+    RSIClosingPrice.append(bitcoinHistoricalData['Data'][i]['close'])
 
-TotalAverageGain, TotalAverageLoss, CurrentChange = calcDailyGains(ClosingPrice, Period)
-finalRSI = calcRSI(TotalAverageGain, TotalAverageLoss, CurrentChange, Period)
+for i in range(0, BandPeriod):
+    #OpeningPrice.append(bitcoinHistoricalData['Data'][i]['open'])
+    BandClosingPrice.append(bitcoinHistoricalData['Data'][i]['close'])
+
+TotalAverageGain, TotalAverageLoss, CurrentChange = calcDailyGains(RSIClosingPrice, RSIPeriod)
+finalRSI = calcRSI(TotalAverageGain, TotalAverageLoss, CurrentChange, RSIPeriod)
 print (finalRSI)
-LowerBand, MiddleBand, UpperBand = calcSMA(ClosingPrice, Period)
+
+LowerBand, MiddleBand, UpperBand = calcSMA(BandClosingPrice, BandPeriod)
+print (LowerBand, MiddleBand, UpperBand)
 
 #Dash Graphing
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+test = []
+test1 = []
+for i in range(0, BandPeriod):
+    test.append(bitcoinHistoricalData['Data'][i]['close'])
+    test1.append(datetime.utcfromtimestamp(bitcoinHistoricalData['Data'][i]['time']).strftime('%Y-%m-%d %H:%M:%S'))
+app.layout = html.Div(children=[
+    html.H1(
+        children='Bitcoin Graph',
+        style={
+            'textAlign': 'center',
+        }
+    ),
+
+    html.Div(children='Charting in style', style={
+        'textAlign': 'center',
+    }),
 
     dcc.Graph(
-        id='example-graph',
+        id='bitcoin-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Scatter(
+                    x = test1,
+                    y = test
+                )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
+            'layout': go.Layout(
+                xaxis={'title' : 'Date'},
+                yaxis={'title': 'Price'}
+            )
         }
     )
 ])
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
